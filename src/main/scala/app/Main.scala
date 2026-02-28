9a18224de9f6b7d98e39498e7d85b1692c7eb9af
Piotr Piecha <piotr.piecha@outlook.com>
2026-02-28 03:00:05 +0100

    each midi track goes to its own queue

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -2,31 +2,52 @@ import cats.effect._
 import fs2.Stream
 import javax.sound.midi._
 
+// to
+
 object Main extends IOApp.Simple {
-
-  def noteOn(note: Int): MidiMessage = {
+  // Utility to create PROGRAM_CHANGE message
+  def programChange(channel: Int, program: Int): MidiMessage = {
     val msg = new ShortMessage()
-    msg.setMessage(ShortMessage.NOTE_ON, 0, note, 100)
+    msg.setMessage(ShortMessage.PROGRAM_CHANGE, channel, program, 0)
     msg
   }
 
-  def noteOff(note: Int): MidiMessage = {
+  def noteOn(note: Int, channel: Int): MidiMessage = {
     val msg = new ShortMessage()
-    msg.setMessage(ShortMessage.NOTE_OFF, 0, note, 0)
+    msg.setMessage(ShortMessage.NOTE_ON, channel, note, 100)
     msg
   }
 
-  def run: IO[Unit] =
-    ReactiveSynth.resource[IO].use { case (queue, synthStream) =>
+  def noteOff(note: Int, channel: Int): MidiMessage = {
+    val msg = new ShortMessage()
+    msg.setMessage(ShortMessage.NOTE_OFF, channel, note, 0)
+    msg
+  }
 
-      val producer =
-        Stream.eval(queue.offer(noteOn(60))) ++
-        Stream.sleep_[IO](scala.concurrent.duration.DurationInt(500).millis) ++
-        Stream.eval(queue.offer(noteOff(60)))
+  def run: IO[Unit] = {
+    // 1. Set instrument (Piano = 0) for each channel before playing notes
+    val producerC: Stream[IO, MidiMessage] =
+      Stream.emit(programChange(0, 0)) ++
+      Stream.emit(noteOn(60, 0)) ++
+      Stream.sleep_[IO](scala.concurrent.duration.DurationInt(500).millis) ++
+      Stream.emit(noteOff(60, 0))
 
-      synthStream
-        .concurrently(producer)
-        .compile
-        .drain
-    }
+    val producerE: Stream[IO, MidiMessage] =
+      Stream.emit(programChange(1, 0)) ++
+      Stream.sleep_[IO](scala.concurrent.duration.DurationInt(100).millis) ++
+      Stream.emit(noteOn(64, 1)) ++
+      Stream.sleep_[IO](scala.concurrent.duration.DurationInt(500).millis) ++
+      Stream.emit(noteOff(64, 1))
+
+    val producerG: Stream[IO, MidiMessage] =
+      Stream.emit(programChange(2, 0)) ++
+      Stream.sleep_[IO](scala.concurrent.duration.DurationInt(200).millis) ++
+      Stream.emit(noteOn(67, 2)) ++
+      Stream.sleep_[IO](scala.concurrent.duration.DurationInt(500).millis) ++
+      Stream.emit(noteOff(67, 2))
+
+    val midiInputs = List(producerC, producerE, producerG)
+
+    ReactiveSynth.resource[IO](midiInputs).use { case (_, all) => all.compile.drain }
+  }
 }
--- src/main/scala/app/midi/ReactiveSynth.scala
+++ src/main/scala/app/midi/ReactiveSynth.scala
@@ -1,36 +1,29 @@
 import cats.effect._
-import cats.effect.std.{Dispatcher, Queue}
+import cats.effect.std.Queue
 import cats.syntax.all._
 import fs2.Stream
 import javax.sound.midi._
 
 object ReactiveSynth {
 
-  def resource[F[_]: Async]: Resource[F, (Queue[F, MidiMessage], Stream[F, Unit])] =
+  def resource[F[_]: Async](inputs: List[Stream[F, MidiMessage]]): Resource[F, (List[Queue[F, Option[MidiMessage]]], Stream[F, Unit])] =
     for {
-      synth <- Resource.make(
-        Async[F].delay(MidiSystem.getSynthesizer)
-      )(s => Async[F].delay(s.close()))
-
-      _ <- Resource.eval(Async[F].delay(synth.open()))
-
-      receiver <- Resource.make(
-        Async[F].delay(synth.getReceiver)
-      )(r => Async[F].delay(r.close()))
-
-      queue <- Resource.eval(Queue.unbounded[F, MidiMessage])
-
+      synth    <- Resource.make(Async[F].delay(MidiSystem.getSynthesizer))(s => Async[F].delay(s.close()))
+      _        <- Resource.eval(Async[F].delay(synth.open()))
+      receiver <- Resource.make(Async[F].delay(synth.getReceiver))(r => Async[F].delay(r.close()))
+      queues   <- Resource.eval(inputs.traverse(_ => Queue.unbounded[F, Option[MidiMessage]]))
     } yield {
-
-      val stream =
-        Stream
-          .fromQueueUnterminated(queue)
-          .evalMap(msg =>
-            Async[F].delay {
-              receiver.send(msg, -1) // -1 = send immediately
-            }
-          )
-
-      (queue, stream)
+      // Each stream puts messages into its own queue
+      val inputStreams = inputs.zip(queues).map { case (stream, queue) =>
+        stream.evalMap(msg => Async[F].delay(println(s"Sending message $msg to queue $queue")) *> queue.offer(Some(msg)))
+      }
+      // Queue streams consume messages and send them to the synthesizer
+      val midiStreams = queues.map { queue =>
+        Stream.fromQueueNoneTerminated(queue).evalMap(msg => Async[F].delay { receiver.send(msg, -1) })
+      }
+      // Start all streams (can be merged or run in parallel)
+      val all = Stream(inputStreams: _*).parJoinUnbounded.merge(Stream(midiStreams: _*).parJoinUnbounded)
+      // You can return just the queues or a tuple (queues, all)
+      (queues, all)
     }
 }
